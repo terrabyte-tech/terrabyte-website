eda7c1aca71098350e6eae06a4d064be2729505c
sharper <hi@sammyharper.com>
2022-12-06 13:55:32 -0600

    added older implementation of Copyright year to shared scripts to be used on Pixel and Pinekeepers sites

--- js/shared-scripts.js
+++ js/shared-scripts.js
@@ -1,13 +1,26 @@
 window.addEventListener("load", function(){
 
   console.log("shared-scripts.js loaded");
-
+  
   //change copyright date  
   var currentDate = new Date();
   var currentYear = currentDate.getFullYear();
   
   var copyrightSpan = document.getElementById("current-year-text");
-  copyrightSpan.appendChild(document.createTextNode(currentYear));
+  // for older implementations
+  var oldCopyrightSpan = document.getElementsByClassName("copyright-date")[0];
+
+  if(copyrightSpan){
+    copyrightSpan.appendChild(document.createTextNode(currentYear));
+  }
+  // for older implementations
+  else if(oldCopyrightSpan){
+    oldCopyrightSpan.appendChild(document.createTextNode(currentYear));
+  }
+  else{
+    // element doesnt exist
+  }
+  
   // 
   
 }, false);
